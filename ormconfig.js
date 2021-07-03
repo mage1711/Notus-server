5ea6c608de254339157e7f4da19a1214ae52abae
mage <ziadaha@hotmail.com>
2021-07-03 15:16:11 +0200

    fixed build issue

---
+++ ormconfig.js
@@ -0,0 +1,20 @@
+const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'build'
+module.exports = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: true,
+  logging: process.env.NODE_ENV === 'development',
+  entities: [rootDir + '/models/**/*{.ts,.js}'],
+  migrations: [rootDir + '/migrations/**/*{.ts,.js}'],
+  subscribers: [rootDir + '/subscribers/**/*{.ts,.js}'],
+  seeds: [rootDir + '/seeds/**/*{.ts,.js}'],
+  cli: {
+    entitiesDir: rootDir + '/models',
+    migrationsDir: rootDir + '/migrations',
+    subscribersDir: rootDir + '/subscribers',
+  },
+}
