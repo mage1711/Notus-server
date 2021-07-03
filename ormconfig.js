7eb27b7220ef726373c6c6377288f59bd4f6ed58
mage <ziadaha@hotmail.com>
2021-07-03 17:45:12 +0200

    fix: database connection

--- ormconfig.js
+++ ormconfig.js
@@ -6,7 +6,7 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: true,
+  synchronize: false,
   ssl: {
     rejectUnauthorized: false,
     ca: process.env.CACERT,
