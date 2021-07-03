aa718dc647558d887dad00db5ae946d69b3c6cdb
mage <ziadaha@hotmail.com>
2021-07-03 15:44:46 +0200

    env fix

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 
 
-const envFound = dotenv.config();
-if (envFound.error) {
+// const envFound = dotenv.config();
+// if (envFound.error) {
 
-  throw new Error("Couldn't find .env file");
-}
+//   throw new Error("Couldn't find .env file");
+// }
 
 export default {
   DATABASE_URL: process.env.MONGODB_URI,
