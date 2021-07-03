8c4a6b672320efaed2c07b0c281b3c2cd54707be
mage <ziadaha@hotmail.com>
2021-07-03 15:47:29 +0200

    env fix

--- build/services/config.js
+++ build/services/config.js
@@ -1,13 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-const envFound = dotenv_1.default.config();
-if (envFound.error) {
-    throw new Error("Couldn't find .env file");
-}
+// const envFound = dotenv.config();
+// if (envFound.error) {
+//   throw new Error("Couldn't find .env file");
+// }
 exports.default = {
     DATABASE_URL: process.env.MONGODB_URI,
     PORT: process.env.PORT,
